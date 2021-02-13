1b2e91daac21a705ab8914b36dcce06577bf07be
max well Ramos <maxii.raamos.carol@gmail.com>
2021-02-13 10:21:18 -0300

    Classe de aplicacao Main

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 public class Aplicacao {
 
 	public static void main(String[] args) throws ParseException {
-		 
-		Scanner sc = new Scanner (System.in);
+
+		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("digite o departamento: ");
 		String departamento = sc.nextLine();
 		System.out.println("Digite os dados do trabalhador: ");
@@ -25,11 +25,10 @@ public class Aplicacao {
 		sc.nextLine();
 		System.out.print("Quantos contratos o trabalhador tem? ");
 		int resposta = sc.nextInt();
-		Funcionario funcionario = new Funcionario(nome,NivelTrabalhador.valueOf(nivelTrabalhador),salarioBase,new Departamento(departamento));
-		
-		
-		
-		for(int i = 0 ; i<resposta; i++) {
+		Funcionario funcionario = new Funcionario(nome, NivelTrabalhador.valueOf(nivelTrabalhador), salarioBase,
+				new Departamento(departamento));
+
+		for (int i = 0; i < resposta; i++) {
 			System.out.println("DIGITE OS DADOS DO CONTRATO: ");
 			System.out.print("Data: ");
 			Date data = sdf.parse(sc.next());
@@ -37,19 +36,19 @@ public class Aplicacao {
 			double valorHora = sc.nextDouble();
 			System.out.print("horas trabalhadas : ");
 			int horasTrabalhadas = sc.nextInt();
-			
-			HoraContrato contrato = new HoraContrato(data,valorHora,horasTrabalhadas);
+
+			HoraContrato contrato = new HoraContrato(data, valorHora, horasTrabalhadas);
 			funcionario.addContrato(contrato);
 		}
 		System.out.println();
 		System.out.println("Digite o mes e o ano da data de pesquisa: MM/yyyy");
-		
-		String mesano= sc.next();
-		int mes =Integer.parseInt( mesano.substring(0,2));
+
+		String mesano = sc.next();
+		int mes = Integer.parseInt(mesano.substring(0, 2));
 		int ano = Integer.parseInt(mesano.substring(3));
-		System.out.println("nome Trabalhador: "+ funcionario.getNome());
-		System.out.println("departamento: "+ funcionario.getDepartamento().getDepartamanto());
-		System.out.println("valor total recebido: " + funcionario.income(ano,mes));
+		System.out.println("nome Trabalhador: " + funcionario.getNome());
+		System.out.println("departamento: " + funcionario.getDepartamento().getDepartamanto());
+		System.out.println("valor total recebido: " + funcionario.income(ano, mes));
 	}
 
 }
